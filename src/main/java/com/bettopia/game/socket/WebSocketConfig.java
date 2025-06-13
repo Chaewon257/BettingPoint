632a6a0f6219f3f83af78e01900a4c9ff9bd789b
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-06-13 20:02:30 +0900

    :sparkles: Feat: 웹소켓 설정

---
+++ src/main/java/com/bettopia/game/socket/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.bettopia.game.socket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+// 웹소켓 설정
+@Configuration
+@EnableWebSocket
+@ComponentScan(basePackages = "com.bettopia") // 또는 포함 경로 지정
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+	private GameWebSocketHandler gameWebSocketHandler;
+	@Autowired
+	private HttpHandshakeInterceptor httpHandshakeInterceptor;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(gameWebSocketHandler, "/ws/game")
+			.setAllowedOrigins("*")
+			.addInterceptors(httpHandshakeInterceptor);
+	}
+}
