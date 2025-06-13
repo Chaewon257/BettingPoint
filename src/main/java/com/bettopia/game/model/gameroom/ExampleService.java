2ff2a426ed43db34b9fbb4d498bb2ed51a9ee1a2
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-06-13 10:46:10 +0900

    ETC: 불필요 파일 삭제

--- src/main/java/com/bettopia/game/model/gameroom/ExampleService.java
+++
@@ -1,8 +0,0 @@
-package com.bettopia.game.model.gameroom;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExampleService {
-
-}
--- src/main/java/com/bettopia/game/model/multi/ExampleService.java
+++
@@ -1,8 +0,0 @@
-package com.bettopia.game.model.multi;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExampleService {
-
-}
