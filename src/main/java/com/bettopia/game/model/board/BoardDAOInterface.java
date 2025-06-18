e3823656037012d02944a1b944328c2d54679b3e
gayun32 <prettygu2355@naver.com>
2025-06-18 20:28:22 +0900

    ✨ feat: BoardDAOInterface 기능 추가

--- src/main/java/com/bettopia/game/model/board/BoardDAOInterface.java
+++ src/main/java/com/bettopia/game/model/board/BoardDAOInterface.java
@@ -2,14 +2,13 @@ package com.bettopia.game.model.board;
 
 import java.util.List;
 
-import com.bettopia.game.model.board.BoardResponseDTO.BoardDTO;
-
 public interface BoardDAOInterface {
-    List<BoardResponseDTO> selectAll();        //게시글 목록 조회
+    List<BoardDTO> selectAll();       			 //게시글 목록 조회
 	void insertBoard(BoardDTO dto); 			//게시글 등록
-	BoardResponseDTO getBoardByUid(String uid);  //게시글 상세 조회
+	BoardDTO getBoardByUid(String uid);  		//게시글 상세 조회
 	void updateBoard(BoardDTO board); 			//게시글 수정
-	void deleteBoard(String uid);  				//게시글 삭제
+	void deleteBoardByUid(String uid);			//게시글 삭제
+	
    
 	
 }
