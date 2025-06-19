7ea1bf394a6cd83170b859f5b18534dd1e007fbb
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-06-19 17:17:10 +0900

    feat: 게임방 입장 페이지 이동 추가

--- src/main/java/com/bettopia/game/controller/GameRoomController.java
+++ src/main/java/com/bettopia/game/controller/GameRoomController.java
@@ -1,7 +1,9 @@
 package com.bettopia.game.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GameRoomController {
     @GetMapping
     public String gameroom() {
-        return "gameroom/gamelist";
+        return "gameroom/gameroomlist";
+    }
+
+    @GetMapping("/detail/{roomId}")
+    public String gameroomDetail(@PathVariable String roomId, Model model) {
+        model.addAttribute("roomId", roomId);
+        return "gameroom/gameroomdetail";
     }
 }
