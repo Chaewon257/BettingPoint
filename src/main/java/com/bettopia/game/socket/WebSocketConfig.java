8f9b5f6ceeb6999fe0f8eb7eaebae1083a460ccd
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-06-19 17:20:48 +0900

    feat: 웹소켓 경로 /ws/game/ 이후 모든 경로로 추가

--- src/main/java/com/bettopia/game/socket/WebSocketConfig.java
+++ src/main/java/com/bettopia/game/socket/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(gameWebSocketHandler, "/ws/game")
+		registry.addHandler(gameWebSocketHandler, "/ws/game/**")
 			.setAllowedOrigins("*")
 			.addInterceptors(httpHandshakeInterceptor);
 	}
