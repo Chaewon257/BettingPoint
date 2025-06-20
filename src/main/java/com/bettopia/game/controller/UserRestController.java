87cdc5e95dc2e50e5b922e8c2a72743e1dbaf67c
A Meaningful star <ghrlfehd123@naver.com>
2025-06-20 11:10:47 +0900

    fix: token을 출력하는 print 삭제

--- src/main/java/com/bettopia/game/controller/UserRestController.java
+++ src/main/java/com/bettopia/game/controller/UserRestController.java
@@ -30,7 +30,6 @@ public class UserRestController {
 		}
 
 		String token = authHeader.substring(7);
-		System.out.println(token);
 		if (!jwtUtil.validateToken(token)) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다.");
 		}
