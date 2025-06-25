18805d73a103dbd22b95b2319f2908c260e63af6
A Meaningful star <ghrlfehd123@naver.com>
2025-06-25 14:13:51 +0900

    edit: Mypage 이동을 위한 Controller

---
+++ src/main/java/com/bettopia/game/controller/MyPageController.java
@@ -0,0 +1,14 @@
+package com.bettopia.game.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class MyPageController {
+	@GetMapping("")
+	public String goLoginPage() {
+		return "/mypage/view";
+	}
+}
