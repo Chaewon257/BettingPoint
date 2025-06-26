46f11d813118ebaf716afe9cc6788089faace89d
YongHee <yonghee1234@gmail.com>
2025-06-26 17:10:24 +0900

    필요없는 코드 삭제

--- src/main/java/com/bettopia/game/controller/TurtleRunRestController.java
+++ src/main/java/com/bettopia/game/controller/TurtleRunRestController.java
@@ -3,21 +3,14 @@ package com.bettopia.game.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bettopia.game.model.auth.AuthService;
-import com.bettopia.game.model.multi.turtle.TurtleRunResultDTO;
-//import com.bettopia.game.model.multi.turtle.TurtleRunService;
 
 @RestController
 @RequestMapping("/api/multi")
