6aaf5e03c474f4f7968467d37c36b16451da93b6
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-06-26 10:51:09 +0900

    :sparkles: feat: 게임/포인트 히스토리 조회/저장 API

---
+++ src/main/java/com/bettopia/game/model/history/HistoryDAO.java
@@ -0,0 +1,71 @@
+package com.bettopia.game.model.history;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Repository
+public class HistoryDAO {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    String namespace = "com.bpoint.history.";
+
+    public List<GameHistoryDTO> gameHistoryList(String userId) {
+        return sqlSession.selectList(namespace + "selectGameHistoryAll", userId);
+    }
+
+    public List<PointHistoryDTO> pointHistoryList(String userId) {
+        return sqlSession.selectList(namespace + "selectPointHistoryAll", userId);
+    }
+
+    public List<GameHistoryDTO> gameHistoryList(String userId, int offset, int size) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("offset", offset);
+        params.put("size", size);
+        params.put("userId", userId);
+        return sqlSession.selectList(namespace + "selectGameHistoryAll", params);
+    }
+
+    public List<PointHistoryDTO> pointHistoryList(String userId, int offset, int size) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("offset", offset);
+        params.put("size", size);
+        params.put("userId", userId);
+        return sqlSession.selectList(namespace + "selectPointHistoryAll", params);
+    }
+
+    public GameHistoryDTO insertGameHistory(GameHistoryDTO gameHistory, String userId) {
+        String uid = UUID.randomUUID().toString().replace("-", "");
+        GameHistoryDTO history = GameHistoryDTO.builder()
+                .uid(uid)
+                .user_uid(userId)
+                .game_uid(gameHistory.getGame_uid())
+                .betting_amount(gameHistory.getBetting_amount())
+                .point_value(gameHistory.getPoint_value())
+                .game_result(gameHistory.getGame_result())
+                .build();
+        sqlSession.insert(namespace + "insertGameHistory", history);
+        return history;
+    }
+
+    public String insertPointHistory(PointHistoryDTO pointHistory, String userId) {
+        String uid = UUID.randomUUID().toString().replace("-", "");
+        PointHistoryDTO history = PointHistoryDTO.builder()
+                .uid(uid)
+                .user_uid(userId)
+                .gh_uid(pointHistory.getGh_uid())
+                .type(pointHistory.getType())
+                .amount(pointHistory.getAmount())
+                .balance_after(pointHistory.getBalance_after())
+                .build();
+        sqlSession.insert(namespace + "insertPointHistory", history);
+        return uid;
+    }
+}
