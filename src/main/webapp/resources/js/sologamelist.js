24180c0bc9efb5a46b5e5bc9500baf5130067ecc
A Meaningful star <ghrlfehd123@naver.com>
2025-07-01 17:39:08 +0900

    edit: 개인 게임 리스트 UI 구현

--- src/main/webapp/resources/js/sologamelist.js
+++ src/main/webapp/resources/js/sologamelist.js
@@ -11,11 +11,12 @@
 	            const s3BaseUrl = "https://bettopia-bucket.s3.ap-southeast-2.amazonaws.com/";
 	            games.forEach(function(game) {
 	                const gameHtml = `
-                    <div class="w-40 flex flex-col items-center border rounded-lg p-4 shadow hover:shadow-md transition cursor-pointer"
-	                			onclick="location.href='/solo/${game.name}'">
-                        <img src="${s3BaseUrl}${game.game_img}" alt="${game.name}" width="200" height="200" class="w-full h-24 object-cover rounded mb-2" />
-                        <span class="text-center text-sm font-semibold">${game.name}</span>
-                    </div>
+						<div class="aspect-[6/7] bg-gray-4 rounded-lg p-2.5 md:p-4 flex flex-col items-center justify-between hover:shadow-[2px_2px_8px_rgba(0,0,0,0.2)]" onclick="location.href='/solo/${game.name}'">
+	                		<div class="aspect-square w-full overflow-hidden">
+                        		<img src="${s3BaseUrl}${game.game_img}" alt="${game.name}" class="w-full rounded" />
+                        	</div>
+                        	<span class="grow flex items-center text-2xl font-extrabold">${game.name}</span>
+                    	</div>
                 `;
 	                container.append(gameHtml);
 	            });
